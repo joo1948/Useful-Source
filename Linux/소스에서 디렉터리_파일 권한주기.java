91c1b7c78df75f445926da568b6a2f73498ea378
Kang JooYoung <joo.zero.10@gmail.com>
2022-12-28 11:08:48 +0900

    Update 소스에서 디렉터리_파일 권한주기.java

--- Linux/소스에서 디렉터리_파일 권한주기.java
+++ Linux/소스에서 디렉터리_파일 권한주기.java
@@ -1,11 +1,6 @@
- String inFileName = APPV_FRONT_PATH + varFileName;
-
- String outpath = APPV_SAVE_PATH + DateHelper.getCurrentDay("yyyyMMdd");
-
- String outFileName =  outpath + "/" + varFileName;
+ // 특수문자 들어갈 수 있기 때문에 구분 + \' 추가
+	String s = "chmod -R 755 \'" + fullFileNamePath +"\'";
+ String[] cmd = {"/bin/sh", "-c",  s};
 
  Runtime.getRuntime().exec("chmod -R 755 " + outpath); //생성한 폴더의 권한 추가
- Runtime.getRuntime().exec("chmod -R 755 " + outFileName);//생성한 파일의 권한 추가
-
-
-
+ Runtime.getRuntime().exec("chmod -R 755 " + cmd);//생성한 파일의 권한 추가
